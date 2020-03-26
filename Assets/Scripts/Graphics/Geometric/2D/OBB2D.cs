92f352e166c57aa359d8033e2f6ec3e0daa2a07e
zhulinzhu <741645596@qq.com>
2020-03-26 16:26:18 +0800

    1

---
+++ Assets/Scripts/Graphics/Geometric/2D/OBB2D.cs
@@ -0,0 +1,226 @@
+﻿using RayGraphics.Math;
+using System.Collections.Generic;
+
+namespace RayGraphics.Geometric
+{
+
+    public class OBB2D
+    {
+        /// <summary>
+        /// 起点
+        /// </summary>
+        public Float2 startPoint;
+        /// <summary>
+        /// 轴1
+        /// </summary>
+        public Float2 aix1;
+        /// <summary>
+        /// 轴2
+        /// </summary>
+        public Float2 aix2;
+        /// <summary>
+        /// obb的大小
+        /// </summary>
+        public Float2 size;
+        /// <summary>
+        /// 设置aabb
+        /// </summary>
+        /// <param name="lb"></param>
+        /// <param name="ru"></param>
+        public void SetObb(Float2 startPoint ,Float2 aix1, Float2 aix2, Float2 size)
+        {
+            this.startPoint = startPoint;
+            this.aix1 = aix1;
+            this.aix2 = aix2;
+            this.size = size;
+            
+        }
+        /// <summary>
+        /// 根据点阵创建obb
+        /// </summary>
+        /// <param name="points"></param>
+        public void Init(Float2[] points)
+        {
+            Float2 centerPos = Float2.zero;
+            Float2[] offPoints = CalcCenter(points, ref centerPos);
+            if (offPoints != null)
+            {
+                Matrix2x2 matrix = Matrix2x2.GetCovarianceMatrix(offPoints);
+                matrix = Matrix2x2.GetFeatureVectorMatrix(matrix);
+                this.aix1 = matrix.col1;
+                this.aix2 = matrix.col2;
+                CalcSize(offPoints, centerPos, matrix);
+            }
+        }
+        /// <summary>
+        /// 计算点阵的中心及修正后的点阵。
+        /// </summary>
+        /// <param name="points"></param>
+        /// <param name="centerPos"></param>
+        /// <returns></returns>
+        private Float2[] CalcCenter(Float2[] points, ref Float2 centerPos)
+        {
+            if (points == null || points.Length <= 0)
+            {
+                centerPos = Float2.zero;
+                return null;
+            }
+            int numPoints = points.Length;
+            Float2[] pVectors = new Float2[numPoints];
+            centerPos = Float2.zero;
+            for (int i = 0; i < numPoints; i++)
+            {
+                pVectors[i] = points[i];
+                centerPos += pVectors[i];
+            }
+            centerPos /= numPoints;
+            for (int i = 0; i < numPoints; i++)
+            {
+                pVectors[i] = pVectors[i] - centerPos;
+            }
+            return pVectors;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="offPoints"></param>
+        /// <param name="centerPos"></param>
+        /// <param name="featreVectorMatrix"></param>
+        private void CalcSize(Float2[] offPoints, Float2 centerPos, Matrix2x2 featreVectorMatrix)
+        {
+            Float2 min = Float2.positiveInfinity;
+            Float2 max = Float2.negativeInfinity;
+            for (int index = 0; index < offPoints.Length; index++)
+            {
+                Float2 vec = offPoints[index];
+
+                min.x = System.Math.Min(min.x, Float2.Dot(vec, featreVectorMatrix.row1));
+                min.y = System.Math.Min(min.y, Float2.Dot(vec, featreVectorMatrix.row2));
+
+                max.x = System.Math.Max(max.x, Float2.Dot(vec, featreVectorMatrix.row1));
+                max.y = System.Math.Max(max.y, Float2.Dot(vec, featreVectorMatrix.row2));
+            }
+            this.startPoint = featreVectorMatrix.col1 * min.x + featreVectorMatrix.col2 * min.y + centerPos;
+            this.size = max - min;
+        }
+        /// <summary>
+        /// 判断点是否在直线上
+        /// </summary>
+        /// <param name="pt"></param>
+        /// <returns></returns>
+        public virtual bool CheckIn(Float2 pt)
+        {
+            return false;
+        }
+        /// <summary>
+        /// 点导几何元素的距离
+        /// </summary>
+        /// <param name="pt"></param>
+        /// <returns></returns>
+        public float CalcDistance(Float2 pt)
+        {
+            return 0;
+        }
+        /// <summary>
+        /// 点导几何元素的投影点
+        /// </summary>
+        /// <param name="pt"></param>
+        /// <returns></returns>
+        public Float2 ProjectPoint(Float2 pt)
+        {
+            return Float2.zero;
+        }
+        /// <summary>
+        /// 求轴向量
+        /// </summary>
+        /// <param name="pt"></param>
+        /// <returns></returns>
+        public Float2 AixsVector(Float2 pt)
+        {
+            return Float2.zero;
+        }
+        /// <summary>
+        /// 与直线的关系
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        public virtual LineRelation CheckLineRelation(Line2D line)
+        {
+            return LineRelation.Detach;
+        }
+        /// <summary>
+        /// 直线与射线间的关系
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        public virtual LineRelation CheckLineRelation(Rays2D line)
+        {
+            return LineRelation.Intersect;
+        }
+        /// <summary>
+        /// 与线段的关系
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        public virtual LineRelation CheckLineRelation(LineSegment2D line)
+        {
+            return LineRelation.Intersect;
+        }
+        /// <summary>
+        /// 镜面但
+        /// </summary>
+        /// <param name="point"></param>
+        /// <returns></returns>
+        public Float2 GetMirrorPoint(Float2 point)
+        {
+            return point - 2 * AixsVector(point);
+        }
+        /// <summary>
+        /// 最短射线包围盒路径
+        /// </summary>
+        /// <param name="line">线段</param>
+        /// <param name="offset">偏移值</param>
+        /// <param name="paths">返回路径</param>
+        /// <returns>true，表示线段与aabb有相交，并返回最短包围路径</returns>
+        public virtual bool RayboundingNearestPath(LineSegment2D line, float offset, ref List<Float2> paths)
+        {
+            return false;
+        }
+        /// <summary>
+        /// 获取挡格附近出生点
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="offset"></param>
+        /// <param name="bornPoint"></param>
+        /// <returns></returns>
+        public virtual bool GetBornPoint(LineSegment2D line, float offset, ref Float2 bornPoint)
+        {
+            return false;
+        }
+        /// <summary>
+        /// 获取交点
+        /// </summary>
+        /// <param name="line"></param>
+        /// <param name="intersectStart"></param>
+        /// <param name="intersectEnd"></param>
+        /// <returns></returns>
+        public virtual bool GetIntersectPoint(LineSegment2D line, ref Float2 intersectStart, ref Float2 intersectEnd)
+        {
+            return false;
+        }
+        /// <summary>
+        /// 求obb的点
+        /// </summary>
+        /// <returns></returns>
+        public Float2[] GetPoints()
+        {
+            Float2[] points = new Float2[4];
+            points[0] = this.startPoint;
+            points[1] = this.startPoint + this.aix1 * this.size.x;
+            points[2] = this.startPoint + this.aix1 * this.size.x + this.aix2 * this.size.y;
+            points[3] = this.startPoint + this.aix2 * this.size.y;
+            return points;
+        }
+    }
+}
--- Assets/Scripts/Graphics/Math/Matrix2x2.cs
+++ Assets/Scripts/Graphics/Math/Matrix2x2.cs
@@ -273,5 +273,131 @@ namespace RayGraphics.Math
         {
             return new Matrix2x2(v.row1, v.row2);
         }
+        /// <summary>
+        /// 计算协方差举证
+        /// https://zhuanlan.zhihu.com/p/37609917
+        /// </summary>
+        /// <param name="points"></param>
+        /// <returns></returns>
+        public static Matrix2x2 GetCovarianceMatrix(Float2[] points)
+        {
+            Matrix2x2 covarianceMatrix = Matrix2x2.zero;
+            if (points == null || points.Length <= 0)
+                return covarianceMatrix;
+            int numPoints = points.Length;
+            //Compute the covariance 
+            for (int col = 0; col < 2; col++)
+            {
+                Float2 data;
+                for (int row = col; row < 2; row++)
+                {
+                    covarianceMatrix[col, row] = 0.0f;
+                    float acc = 0.0f;
+                    //cov(X,Y)=E[(X-x)(Y-y)]
+                    for (int i = 0; i < numPoints; i++)
+                    {
+                        data = points[i];
+                        acc += data[col] * data[row];
+                    }
+                    acc /= numPoints;
+                    covarianceMatrix[col, row] = acc;
+                    //symmetric
+                    covarianceMatrix[row, col] = acc;
+                }
+            }
+            return covarianceMatrix;
+        }
+        /// <summary>
+        /// 求特征向量矩阵 通过使用Jacobi 迭代算法
+        /// </summary>
+        /// <param name="matrix"></param>
+        public static Matrix2x2 GetFeatureVectorMatrix(Matrix2x2 matrix)
+        {
+            float p, q, spq;
+            float cosa, sina;  //cos(alpha) and sin(alpha)
+            float temp;
+            float s1 = 0.0f;    //sums of squares of diagonal
+            float s2;          //elements
+
+            bool flag = true;  //determine whether to iterate again
+            int iteration = 0;   //iteration counter
+
+            float[] data = new float[2];
+            Matrix2x2 t = Matrix2x2.one;//To store the product of the rotation matrices.
+            do
+            {
+                iteration++;
+                for (int i = 0; i < 2; i++)
+                {
+                    for (int j = i + 1; j < 2; j++)
+                    {
+                        if (System.Math.Abs(matrix[j, i]) < MathUtil.kEpsilon)
+                            matrix[j, i] = 0.0f;
+                        else
+                        {
+                            q = System.Math.Abs(matrix[i, i] - matrix[j, j]);
+                            if (q > MathUtil.kEpsilon)
+                            {
+                                p = (2.0f * matrix[j, i] * q) / (matrix[i, i] - matrix[j, j]);
+                                spq = (float)System.Math.Sqrt(p * p + q * q);
+                                cosa = (float)System.Math.Sqrt((1.0f + q / spq) / 2.0f);
+                                sina = p / (2.0f * cosa * spq);
+                            }
+                            else
+                                sina = cosa = (float)System.Math.Sqrt(2) * 0.5f;
+
+                            for (int k = 0; k < 2; k++)
+                            {
+                                temp = t[i, k];
+                                t[i, k] = (temp * cosa + t[j, k] * sina);
+                                t[j, k] = (temp * sina - t[j, k] * cosa);
+                            }
+                            for (int k = i; k < 2; k++)
+                            {
+                                if (k > j)
+                                {
+                                    temp = matrix[k, i];
+                                    matrix[k, i] = (cosa * temp + sina * matrix[k, j]);
+                                    matrix[k, j] = (sina * temp - cosa * matrix[k, j]);
+                                }
+                                else
+                                {
+                                    data[k] = matrix[k, i];
+                                    matrix[k, i] = (cosa * data[k] + sina * matrix[j, k]);
+
+                                    if (k == j)
+                                        matrix[k, j] = (sina * data[k] - cosa * matrix[k, j]);
+                                }
+                            }
+                            data[j] = sina * data[i] - cosa * data[j];
+
+                            for (int k = 0; k <= j; k++)
+                            {
+                                if (k <= i)
+                                {
+                                    temp = matrix[i, k];
+                                    matrix[i, k] = (cosa * temp + sina * matrix[j, k]);
+                                    matrix[j, k] = (sina * temp - cosa * matrix[j, k]);
+
+                                }
+                                else
+                                    matrix[j, k] = (sina * data[k] - cosa * matrix[j, k]);
+
+                            }
+                        }
+                    }
+                }
+                s2 = 0.0f;
+                for (int i = 0; i < 2; i++)
+                {
+                    s2 += matrix[i, i] * matrix[i, i];
+                }
+                if (System.Math.Abs(s2) < MathUtil.kEpsilon || System.Math.Abs(1 - s1 / s2) < MathUtil.kEpsilon)
+                    flag = false;
+                else
+                    s1 = s2;
+            } while (flag);
+            return t;
+        }
     }
 }
