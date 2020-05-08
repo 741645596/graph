a279b4673d7205abaa3247b0ffc027f9fed1556d
zhulinzhu <741645596@qq.com>
2020-05-08 11:40:58 +0800

    1

--- Assets/Scripts/Graphics/Geometric/2D/ConvexPolygon2D.cs
+++ Assets/Scripts/Graphics/Geometric/2D/ConvexPolygon2D.cs
@@ -14,13 +14,13 @@ namespace RayGraphics.Geometric
         /// </summary>
         /// <param name="listpt"></param>
         /// <returns></returns>
-        public static List<int> GenerateConvexPolygonIndex(Float2[] listpt)
+        public static List<int> GenerateConvexPolygonIndex(Double2[] listpt)
         {
             List<int> listIndex = new List<int>();
-            List<Float2> listPoint= GenerateConvexPolygon(listpt);
+            List<Double2> listPoint= GenerateConvexPolygon(listpt);
             if (listPoint == null || listPoint.Count == 0)
                 return listIndex;
-            foreach (Float2 pos in listPoint)
+            foreach (Double2 pos in listPoint)
             {
                 int index = GetIndex(pos, listpt);
                 if (index != -1)
@@ -30,7 +30,7 @@ namespace RayGraphics.Geometric
             }
             return listIndex;
         }
-        private static int GetIndex(Float2 pos, Float2[] listpt)
+        private static int GetIndex(Double2 pos, Double2[] listpt)
         {
             if (listpt == null || listpt.Length == 0)
                 return -1;
@@ -47,64 +47,64 @@ namespace RayGraphics.Geometric
         /// </summary>
         /// <param name="listpt"></param>
         /// <returns></returns>
-        public static List<Float2> GenerateConvexPolygon(Float2[] listpt)
+        public static List<Double2> GenerateConvexPolygon(Double2[] listpt)
         {
             if (listpt == null || listpt.Length == 0)
                 return null;
             // 先生成aabb。
-            Float2 min = Float2.positiveInfinity;
-            Float2 max = Float2.negativeInfinity;
+            Double2 min = Double2.positiveInfinity;
+            Double2 max = Double2.negativeInfinity;
 
-            foreach (Float2 pos in listpt)
+            foreach (Double2 pos in listpt)
             {
-                min = Float2.Min(min, pos);
-                max = Float2.Max(max, pos);
+                min = Double2.Min(min, pos);
+                max = Double2.Max(max, pos);
             }
             // 收集条边上的点，按顺时针收集。lb放置在bottom边上，rb 放置在right边上，ru顶点放置up边上, lu顶点放置在left边上
-            Float2[][] edgePointArray = new Float2[4][];
+            Double2[][] edgePointArray = new Double2[4][];
             for (int i = 0; i < 4; i++)
             {
-                edgePointArray[i] = new Float2[2];
+                edgePointArray[i] = new Double2[2];
                 if (i < 2)
                 {
-                    edgePointArray[i][0] = Float2.positiveInfinity;
-                    edgePointArray[i][1] = Float2.negativeInfinity;
+                    edgePointArray[i][0] = Double2.positiveInfinity;
+                    edgePointArray[i][1] = Double2.negativeInfinity;
                 }
                 else 
                 {
-                    edgePointArray[i][0] = Float2.negativeInfinity;
-                    edgePointArray[i][1] = Float2.positiveInfinity;
+                    edgePointArray[i][0] = Double2.negativeInfinity;
+                    edgePointArray[i][1] = Double2.positiveInfinity;
                 }
             }
             // 不在边上的顶点
-            List<Float2> listOutEdgePoint = new List<Float2>();
+            List<Double2> listOutEdgePoint = new List<Double2>();
             // 进行收集了。
-            foreach (Float2 pos in listpt)
+            foreach (Double2 pos in listpt)
             {
                 bool isInedge = false;
                 // 第一条边 bottom edge
                 if (pos.y == min.y)
                 {
-                    edgePointArray[0][0] = Float2.Min(pos, edgePointArray[0][0]);
-                    edgePointArray[0][1] = Float2.Max(pos, edgePointArray[0][1]);
+                    edgePointArray[0][0] = Double2.Min(pos, edgePointArray[0][0]);
+                    edgePointArray[0][1] = Double2.Max(pos, edgePointArray[0][1]);
                     isInedge = true;
                 }
                 if (pos.x == max.x) //  right edge
                 {
-                    edgePointArray[1][0] = Float2.Min(pos, edgePointArray[1][0]);
-                    edgePointArray[1][1] = Float2.Max(pos, edgePointArray[1][1]);
+                    edgePointArray[1][0] = Double2.Min(pos, edgePointArray[1][0]);
+                    edgePointArray[1][1] = Double2.Max(pos, edgePointArray[1][1]);
                     isInedge = true;
                 }
                 if(pos.y == max.y ) //  up edge
                 {
-                    edgePointArray[2][0] = Float2.Max(pos, edgePointArray[2][0]);
-                    edgePointArray[2][1] = Float2.Min(pos, edgePointArray[2][1]);
+                    edgePointArray[2][0] = Double2.Max(pos, edgePointArray[2][0]);
+                    edgePointArray[2][1] = Double2.Min(pos, edgePointArray[2][1]);
                     isInedge = true;
                 }
                 if(pos.x == min.x) //  left edge
                 {
-                    edgePointArray[3][0] = Float2.Max(pos, edgePointArray[3][0]);
-                    edgePointArray[3][1] = Float2.Min(pos, edgePointArray[3][1]);
+                    edgePointArray[3][0] = Double2.Max(pos, edgePointArray[3][0]);
+                    edgePointArray[3][1] = Double2.Min(pos, edgePointArray[3][1]);
                     isInedge = true;
                 }
                 if(isInedge == false)
@@ -114,9 +114,9 @@ namespace RayGraphics.Geometric
             }
             // 边上的顶点按顺时针排序了。
             // 
-            List<Float2> listPoly = new List<Float2>();
-            Float2 startPoint ;
-            Float2 endPoint = Float2.negativeInfinity;
+            List<Double2> listPoly = new List<Double2>();
+            Double2 startPoint ;
+            Double2 endPoint = Double2.negativeInfinity;
             for (int i = 0; i < 4; i++)
             {
                 startPoint = edgePointArray[i][0];
@@ -131,7 +131,7 @@ namespace RayGraphics.Geometric
                 }
                 if (endPoint != edgePointArray[(i + 1) % 4][0])
                 {
-                    List<Float2> llinkPoint = SearchOutPoint(i, endPoint, edgePointArray[(i + 1) % 4][0], listOutEdgePoint);
+                    List<Double2> llinkPoint = SearchOutPoint(i, endPoint, edgePointArray[(i + 1) % 4][0], listOutEdgePoint);
                     if (llinkPoint != null && llinkPoint.Count > 0)
                     {
                         listPoly.AddRange(llinkPoint);
@@ -160,56 +160,56 @@ namespace RayGraphics.Geometric
         /// <param name="e"></param>
         /// <param name="listOutEdgePoint"></param>
         /// <returns></returns>
-        private static List<Float2> SearchOutPoint(int edgeIndex, Float2 s, Float2 e, List<Float2> listOutEdgePoint)
+        private static List<Double2> SearchOutPoint(int edgeIndex, Double2 s, Double2 e, List<Double2> listOutEdgePoint)
         {
             if (listOutEdgePoint == null || listOutEdgePoint.Count == 0)
                 return null;
 
-            Float2 outdir;
-            Float2 indir;
+            Double2 outdir;
+            Double2 indir;
             if (edgeIndex == 0)
             {
                 if (s.y == e.y)
                     return null;
-                outdir = Float2.right;
+                outdir = Double2.right;
                 indir = (e - s).normalized;
             }
             else if (edgeIndex == 1)
             {
                 if (s.x == e.x)
                     return null;
-                outdir = Float2.up;
+                outdir = Double2.up;
                 indir = (e - s).normalized;
             }
             else if (edgeIndex == 2)
             {
                 if (s.y == e.y)
                     return null;
-                outdir = Float2.left;
+                outdir = Double2.left;
                 indir = (e - s).normalized;
             }
             else if (edgeIndex == 3)
             {
                 if (s.x == e.x)
                     return null;
-                outdir = Float2.down;
+                outdir = Double2.down;
                 indir = (e - s).normalized;
             }
             else return null;
             // 开始搜寻了。
-            List<Float2> lResult = new List<Float2>();
-            Float2 startPoint = s;
-            Float2 bestPoint = Float2.zero;
+            List<Double2> lResult = new List<Double2>();
+            Double2 startPoint = s;
+            Double2 bestPoint = Double2.zero;
             bool isHaveBestPoint = false;
-            List<Float2> listinPoints = new List<Float2>();
-            List<Float2> lHave = listOutEdgePoint;
+            List<Double2> listinPoints = new List<Double2>();
+            List<Double2> lHave = listOutEdgePoint;
 
             while (lHave != null && lHave.Count > 0)
             {
                 // 先过滤了。
-                foreach (Float2 pos in lHave)
+                foreach (Double2 pos in lHave)
                 {
-                    if (Float2.CheckPointInCorns(pos, startPoint, indir, outdir) == true)
+                    if (Double2.CheckPointInCorns(pos, startPoint, indir, outdir) == true)
                     {
                         listinPoints.Add(pos);
                         if (isHaveBestPoint == false)
@@ -225,7 +225,7 @@ namespace RayGraphics.Geometric
                     for (int i = 1; i < listinPoints.Count; i++)
                     {
                         // 比较更好的点。
-                        if (Float2.CheckPointInCorns(listinPoints[i], startPoint, (bestPoint - startPoint).normalized, outdir) == true)
+                        if (Double2.CheckPointInCorns(listinPoints[i], startPoint, (bestPoint - startPoint).normalized, outdir) == true)
                         {
                             bestPoint = listinPoints[i];
                         }
@@ -236,7 +236,7 @@ namespace RayGraphics.Geometric
                     indir = (e - startPoint).normalized;
                     listinPoints.Remove(bestPoint);
                     lHave = listinPoints;
-                    listinPoints = new List<Float2>();
+                    listinPoints = new List<Double2>();
                     isHaveBestPoint = false;
                 }
                 else break;
