38fe2103da505d0583a8636dfff2333304daabcc
zhulinzhu <741645596@qq.com>
2020-05-22 10:59:33 +0800

    1

---
+++ Assets/Scripts/Graphics/Geometric/2D/Point2D.cs
@@ -0,0 +1,30 @@
+﻿using RayGraphics.Math;
+
+namespace RayGraphics.Geometric
+{
+    /// <summary>
+    /// 顶点组
+    /// </summary>
+    [System.Serializable]
+    public struct Point2D
+    {
+        /// <summary>
+        /// 直线上得点
+        /// </summary>
+        public Double2 startPoint;
+        /// <summary>
+        /// 直线上得点
+        /// </summary>
+        public Double2 endPoint;
+        /// <summary>
+        /// 构建线段
+        /// </summary>
+        /// <param name="startPt"></param>
+        /// <param name="endPt"></param>
+        public Point2D(Double2 startPt, Double2 endPt)
+        {
+            this.startPoint = startPt;
+            this.endPoint = endPt;
+        }
+    }
+}
