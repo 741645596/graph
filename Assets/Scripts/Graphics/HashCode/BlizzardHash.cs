8205b15649cacee544e710f895768c60956a0269
zhulinzhu <741645596@qq.com>
2020-10-08 10:35:55 +0800

    1

---
+++ Assets/Scripts/Graphics/HashCode/BlizzardHash.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace RayGraphics.Math
+{
+    /// <summary>
+    /// 暴雪 hash 算法one-way hash 
+    /// </summary>
+    public class BlizzardHash
+    {
+        /// <summary>
+        /// 表
+        /// </summary>
+        private uint[] m_CryptTable = new uint[0x500];
+        /// <summary>
+        /// 生成hash
+        /// </summary>
+        public void PrepareTable()
+        {
+            uint seed = 0x00100001;
+            uint index1 ;
+            uint index2 ;
+            uint i ;
+            for (index1 = 0; index1 < 0x100; index1++)
+            {
+                for (index2 = index1, i = 0; i < 5; i++, index2 += 0x100)
+                {
+                    uint temp1, temp2;
+                    seed = (seed * 125 + 3) % 0x2AAAAB;
+                    temp1 = (seed & 0xFFFF) << 0x10;
+                    seed = (seed * 125 + 3) % 0x2AAAAB;
+                    temp2 = (seed & 0xFFFF);
+                    m_CryptTable[index2] = (temp1 | temp2);
+                }
+            }
+        }
+        /// <summary>
+        ///  Blizzard的这个算法是非常高效的，被称为"One-Way Hash"
+        ///  A one-way hash is a an algorithm that is constructed in such a way 
+        ///  that deriving the original string (set of strings, actually) is virtually impossible
+        /// </summary>
+        /// <param name="strValue"></param>
+        /// <param name="dwHashType">hash 类型</param>
+        /// <returns></returns>
+        public uint HashString(string strValue, uint dwHashType)
+        {
+            uint seed1 = 0x7FED7FED;
+            uint seed2 = 0xEEEEEEEE;
+            uint ch;
+            foreach (char c in strValue)
+            {
+                ch = c;
+                // 因为是作为文件系统使用，所以才用字符串大小不敏感。
+                if (ch >= 'a' && ch <= 'z')
+                {
+                    ch = ch + 'A' - 'a';
+                }
+                seed1 = m_CryptTable[(dwHashType << 8) + ch] ^ (seed1 + seed2);
+                seed2 = ch + seed1 + seed2 + (seed2 << 5) + 3;
+            }
+            return seed1;
+        }
+        /// <summary>
+        /// 查找hash所在的位置
+        /// </summary>
+        /// <param name="strValue"></param>
+        /// <param name="lpTable"></param>
+        /// <param name="nTableSize"></param>
+        /// <returns></returns>
+        public int GetHashTablePos(string strValue, SomeHash[] lpTable, int nTableSize)
+        {
+            uint hashOffset = 0;
+            uint hashA = 1;
+            uint hashB = 2;
+            //计算出字符串的三个哈希值（一个用来确定位置，另外两个用来校验)
+            uint nHash = HashString(strValue, hashOffset);
+            uint nHashA = HashString(strValue, hashA);
+            uint nHashB = HashString(strValue, hashB);
+
+            int nHashStart = (int)(nHash % nTableSize);
+            int nHashPos = nHashStart;
+
+            while (lpTable[nHashPos].bExists)
+            {
+                // 检查2个hash值是否匹对。
+                if (lpTable[nHashPos].nHashA == nHashA && lpTable[nHashPos].nHashB == nHashB)
+                {
+                    return nHashPos;
+                }
+                else // 找到下一个位置。
+                {
+                    nHashPos = (nHashPos + 1) % nTableSize;
+                }
+
+                if (nHashPos == nHashStart)
+                    break;
+            }
+            return -1;
+        }
+    }
+    /// <summary>
+    /// 相同的hash
+    /// </summary>
+    public struct SomeHash
+    {
+        public int nHashA;
+        public int nHashB;
+        public bool bExists;
+    };
+}
---
+++ Assets/Scripts/Graphics/HashCode/HashCodeHelp.cs
@@ -0,0 +1,211 @@
+﻿using System;
+/// <summary>
+/// https://blog.csdn.net/pkueecser/article/details/15504753
+/// </summary>
+namespace RayGraphics.Math
+{
+    /// <summary>
+    /// hash函数
+    /// </summary>
+    public class HashCodeHelp 
+    {
+        /// <summary>
+        /// String中hashCode方法
+        /// 算法：s[0]*31^(n-1) + s[1]*31^(n-2) + … + s[n-1]
+        /// </summary>
+        /// <returns></returns>
+        public int hashCode(string value)
+        {
+            int hash = 0;
+            if (hash == 0 && value.Length > 0)
+            {
+                for (int i = 0; i < value.Length; i++)
+                {
+                    hash = 31 * hash + value[i];
+                }
+            }
+            return hash;
+        }
+
+        /// <summary>
+        /// 一个比较好的字符串hash算法，add到这里，听说是微软用
+        /// 这个算法要保证str的长度是8的倍数，如果不够用0补齐，
+        /// 不然会导致越界，计算出的hash就不准确了。
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        
+        uint getHashCode(string value)
+        {
+            /*uint32_t chPtr[MAX_LEN];
+                memset(chPtr,0,sizeof(chPtr));
+                int length = strlen(str);
+                memcpy(chPtr, str, length);
+                UINT32 num = 0x15051505;
+                UINT32 num2 = num;
+                UINT32* numPtr = (UINT32*)chPtr;
+                int i = 0;
+            for(i=length;i>0;i-=4)
+            {   
+                num = (((num << 5) + num) + (num >> 0x1b)) ^ numPtr[0];
+                if(i<=2) break;
+                num2=(((num2 << 5) + num2) + (num2 >> 0x1b)) ^ numPtr[1];
+                numPtr+=2;
+            }   
+            return (num + (num2* 0x5d588b65));*/
+            return 0;
+        }
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public uint SDBMHash(string value)
+        {
+            uint hash = 0;
+            foreach(char c in value)
+            {
+                hash = c + (hash << 6) + (hash << 16) - hash;
+            }
+            return (hash & 0x7FFFFFFF);
+        }
+        /// <summary>
+        /// RS Hash Function
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public uint RSHash(string value)
+        {
+            uint b = 378551;
+            uint a = 63689;
+            uint hash = 0;
+
+            foreach (char c in value)
+            {
+                hash = hash * a + c;
+                a *= b;
+            }
+            return (hash & 0x7FFFFFFF);
+        }
+        /// <summary>
+        /// JS Hash Function
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public uint JSHash(string value)
+        {
+            uint hash = 1315423911;
+            foreach (char c in value)
+            {
+                hash ^= ((hash << 5) + c + (hash >> 2));
+            }
+
+            return (hash & 0x7FFFFFFF);
+        }
+        /// <summary>
+        /// P. J. Weinberger Hash Function
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public uint PJWHash(string value)
+        {
+            uint hash = 0;
+            /*uint BitsInUnignedInt = (unsigned int)(sizeof(unsigned int) *8);
+            uint ThreeQuarters = (unsigned int)((BitsInUnignedInt * 3) / 4);
+            uint OneEighth = (unsigned int)(BitsInUnignedInt / 8);
+            uint HighBits = (unsigned int)(0xFFFFFFFF) << (BitsInUnignedInt - OneEighth);
+            uint hash = 0;
+            uint test = 0;
+
+            foreach (char c in value)
+            {
+                hash = (hash << OneEighth) + c;
+                if ((test = hash & HighBits) != 0)
+                {
+                    hash = ((hash ^ (test >> ThreeQuarters)) & (~HighBits));
+                }
+            }*/
+            return (hash & 0x7FFFFFFF);
+        }
+        /// <summary>
+        /// ELF Hash Function
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public uint ELFHash(string value)
+        {
+            uint hash = 0;
+            uint x;
+            foreach (char c in value)
+            {
+                hash = (hash << 4) + c;
+                if ((x = hash & 0xF0000000) != 0)
+                {
+                    hash ^= (x >> 24);
+                    hash &= ~x;
+                }
+            }
+            return (hash & 0x7FFFFFFF);
+        }
+        /// <summary>
+        /// BKDR Hash Function
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public uint BKDRHash(string value)
+        {
+            uint seed = 131; // 31 131 1313 13131 131313 etc..
+            uint hash = 0;
+
+            foreach (char c in value)
+            {
+                hash = hash * seed + c;
+            }
+
+            return (hash & 0x7FFFFFFF);
+        }
+
+        /// <summary>
+        /// DJB Hash Function
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public uint DJBHash(string value)
+        {
+            uint hash = 5381;
+
+            foreach (char c in value)
+            {
+                hash += (hash << 5) + c;
+            }
+
+            return (hash & 0x7FFFFFFF);
+        }
+        /// <summary>
+        /// AP Hash Function
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public uint APHash(string value)
+        {
+            uint hash = 0;
+            int i =0;
+
+            foreach (char c in value)
+            {
+                if ((i & 1) == 0)
+                {
+                    hash ^= ((hash << 7) ^ c ^ (hash >> 3));
+                }
+                else
+                {
+                    hash ^= (~((hash << 11) ^ c ^ (hash >> 5)));
+                }
+                i++;
+            }
+
+            return (hash & 0x7FFFFFFF);
+        }
+    }
+}
