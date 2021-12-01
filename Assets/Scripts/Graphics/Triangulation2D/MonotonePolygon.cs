102fc912027003b51f23e827589b78be32dbfce2
zhulinzhu <741645596@qq.com>
2021-12-01 11:05:37 +0800

    1

--- Assets/Scripts/Graphics/Triangulation2D/MonotonePolygon.cs
+++ Assets/Scripts/Graphics/Triangulation2D/MonotonePolygon.cs
@@ -11,10 +11,78 @@ namespace RayGraphics.Triangulation
         /// <summary>
         /// 构造函数
         /// </summary>
-        /// <param name="listPoints"></param>
+        /// <param name="listPoints">为逆时针序列,且为单调多边形</param>
         public MonotonePolygon(List<VertexInfo> listPoints)
         {
+            if (listPoints == null)
+                return;
+            int count = listPoints.Count;
+            if (count < 3)
+                return;
+            // 找最小
+            int minIndex = -1;
+            for (int i = 0; i < count; i++)
+            {
+                int prev = (i - 1) < 0 ? (count - 1):(i - 1);
+                int next = (i + 1) >= count ? 0 : (i + 1);
+                if (listPoints[i].pos.y < listPoints[prev].pos.y && listPoints[i].pos.y <= listPoints[next].pos.y)
+                {
+                    minIndex = i;
+                    break;
+                }
+            }
+            if (minIndex == -1)
+                return;
 
+            // 找最大
+            int maxIndex = -1;
+            for (int i = 0; i < count; i++)
+            {
+                int prev = (i - 1) < 0 ? (count - 1) : (i - 1);
+                int next = (i + 1) >= count ? 0 : (i + 1);
+                if (listPoints[i].pos.y >= listPoints[prev].pos.y && listPoints[i].pos.y > listPoints[next].pos.y)
+                {
+                    maxIndex = i;
+                    break;
+                }
+            }
+            if (maxIndex == -1)
+                return;
+            // 得到单调链
+            if (maxIndex > minIndex)
+            {
+                // left
+                for (int i = maxIndex + 1; i < count; i++)
+                {
+                    this.left.Add(new TriVertexInfo(listPoints[i], true));
+                }
+                for (int i = 0; i < minIndex; i++)
+                {
+                    this.left.Add(new TriVertexInfo(listPoints[i], true));
+                }
+                // right
+                for (int i = maxIndex ; i >= minIndex; i--)
+                {
+                    this.right.Add(new TriVertexInfo(listPoints[i], false));
+                }
+            }
+            else if (maxIndex < minIndex)
+            {
+                // left
+                for (int i = maxIndex + 1; i < minIndex; i++)
+                {
+                    this.left.Add(new TriVertexInfo(listPoints[i], true));
+                }
+                // right
+                for (int i = maxIndex; i >= 0; i--)
+                {
+                    this.right.Add(new TriVertexInfo(listPoints[i], false));
+                }
+                for (int i = 0; i <= minIndex; i++)
+                {
+                    this.right.Add(new TriVertexInfo(listPoints[i], false));
+                }
+            }
         }
         /// <summary>
         /// 单调链- left -单调降
@@ -105,6 +173,12 @@ namespace RayGraphics.Triangulation
 
     public class TriVertexInfo
     {
+        public TriVertexInfo(VertexInfo v, bool isLeft)
+        {
+            this.pos = v.pos;
+            this.index = v.index;
+            this.isLeft = isLeft;
+        }
         /// <summary>
         /// 顶点数据
         /// </summary>
